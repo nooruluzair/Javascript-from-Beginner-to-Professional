5698684bb6f9f84447a44be6d2c038760ba66568
Noor ul Ain <57191809+nooruluzair@users.noreply.github.com>
2022-10-22 20:42:23 +0500

    Update primitive data types.js

--- Primitive data types/primitive data types.js
+++ Primitive data types/primitive data types.js
@@ -3,4 +3,12 @@ let namewithuzair = "Noor ul Uzair";    //with double quotes
 let myname = "Noor";
 let printname = `My name is ${myname}`; //with backtics pointing toward a variable
 
+
+let str = "He asked,\"What is your name\"?";
+/*ignoring double quotes inside the sentense using backlash*/
+
+let str1 = 'What\'s your problem?';
+/*ignoring double quotes inside the sentense using backlash*/
+
 console.log(name1, namewithuzair,printname);
+console.log(str,str1);
