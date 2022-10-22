2cfeaa3e77992256c6d6abc0e1d53d499134ce79
Noor ul Ain <57191809+nooruluzair@users.noreply.github.com>
2022-10-22 20:21:56 +0500

    Create primitive data types.js

---
+++ Primitive data types/primitive data types.js
@@ -0,0 +1,6 @@
+let name1 = 'Noor ul Ain';              //with single quotes
+let namewithuzair = "Noor ul Uzair";    //with double quotes
+let myname = "Noor";
+let printname = `My name is ${myname}`; //with backtics pointing toward a variable
+
+console.log(name1, namewithuzair,printname);
