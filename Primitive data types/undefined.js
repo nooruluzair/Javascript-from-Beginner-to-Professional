eb923a665392421c50ea1678d48ac2733df93da4
Noor ul Ain <57191809+nooruluzair@users.noreply.github.com>
2022-10-24 23:51:53 +0500

    Update undefined.js

--- Primitive data types/undefined.js
+++ Primitive data types/undefined.js
@@ -1,2 +1,11 @@
 let nothingIsAssignedtoThisVariable;
 console.log(nothingIsAssignedtoThisVariable);
+
+
+let notRecommended = undefined;
+let notDefined;
+
+console.log("are they equal?", notRecommended === notDefined); 
+
+let recommended = null;
+console.log("are they equal?",recommended === notDefined);
