d400edb80b824eb6329b2d953402f8d70c55a952
Noor ul Ain <57191809+nooruluzair@users.noreply.github.com>
2022-10-26 00:27:10 +0500

    Create converting data types.js

---
+++ modifying data type/converting data types.js
@@ -0,0 +1,19 @@
+let num1 = 3;
+let num2 = "3"; //java changing string to number and giving output 9
+console.log(num1 * num2); 
+
+let num3 = 3;
+let num4 = "3"; //java concatenate and giving output 33, it is changing number into string
+console.log(num3 + num4); 
+
+let numtoStr = 5;
+numtoStr = String(numtoStr);
+console.log(numtoStr, typeof(numtoStr));
+
+let StrtoNum = "Noor";
+StrtoNum = Number(StrtoNum);
+console.log(StrtoNum, typeof(StrtoNum));
+
+let StrtoNum3 = "";
+StrtoNum3 = Number(StrtoNum3);
+console.log(StrtoNum3, typeof(StrtoNum3));
