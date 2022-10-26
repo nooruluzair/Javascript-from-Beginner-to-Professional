9a317e1f915fa768a83e3f7d6fc062756cb37483
Noor ul Ain <57191809+nooruluzair@users.noreply.github.com>
2022-10-26 18:45:27 +0500

    Create Practice exercise 2.1.js

---
+++ modifying data type/Practice exercise 2.1.js
@@ -0,0 +1,11 @@
+let str1 = 'Laurence';
+let str2 = "Svekis";
+let val1 = undefined;
+let val2 = null;
+let myNum = 1000;
+
+console.log("Laurence", typeof str1);
+console.log("Svekis", typeof str2);
+console.log("undefined", typeof val1);
+console.log("null", typeof val2);
+console.log("1000", typeof myNum);
