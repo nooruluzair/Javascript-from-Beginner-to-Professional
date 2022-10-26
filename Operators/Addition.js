d2a5864b4ac34dd3b0408bcf1219fdfd3a44e2d3
Noor ul Ain <57191809+nooruluzair@users.noreply.github.com>
2022-10-26 23:11:04 +0500

    addition

---
+++ Operators/Addition.js
@@ -0,0 +1,8 @@
+let num1 = 3;
+let num2 = 4;
+console.log(num1 + num2); //Addition
+
+let str1 = "Noor ul";
+let str2 = "Ain";
+console.log(str1 + str2); //concatenate
+
---
+++ Operators/Practice exercise 2.2.js
@@ -0,0 +1,5 @@
+let myname = "Noor ul Ain";
+let age = 32;
+let condition = true;
+
+console.log("Hello, my is " + myname + " and I am " + age + " years old and I can code JavaScript: " + condition);
