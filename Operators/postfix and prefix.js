a175daf86112b21a72b57e2dfde5f44e3c6bb323
Noor ul Ain <57191809+nooruluzair@users.noreply.github.com>
2022-10-31 23:13:24 +0500

    operator

---
+++ Operators/postfix and prefix.js
@@ -0,0 +1,9 @@
+nb1 = 5;
+console.log(nb1++); //first it send the variable and than do the incrment
+console.log(++nb1); //recieved 6 from above and than first increment and than print varaible
+
+nb1 = 3;
+nb2 = 2;
+nb3 = 5;
+console.log(nb1++ + ++nb3 * nb2++);
+
---
+++ Operators/unary operator.js
@@ -0,0 +1,7 @@
+nb1 = 5;
+nb1++;
+console.log(nb1);
+
+nb1 = 5;
+nb1--;
+console.log(nb1);
