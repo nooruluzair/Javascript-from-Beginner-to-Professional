24058450c2c5bb8ea7b04605efbbf18f50c5be2c
Noor ul Ain <57191809+nooruluzair@users.noreply.github.com>
2022-10-31 19:02:39 +0500

    operators

---
+++ Operators/Division.js
@@ -0,0 +1,4 @@
+let nb1 = 30;
+let nb2 = 24;
+let result = nb1/nb2;
+console.log(result)
---
+++ Operators/multiplication.js
@@ -0,0 +1,6 @@
+let nb1 = 30;
+let nb2 = 24;
+let nb3 = 26;
+let str6 = "Pak";
+console.log(nb1 * nb2);
+console.log(nb3 * str6);
