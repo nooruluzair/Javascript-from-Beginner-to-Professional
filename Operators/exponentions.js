47cb6fee2fab74704d261d82e168743b326bbdd3
Noor ul Ain <57191809+nooruluzair@users.noreply.github.com>
2022-10-31 19:18:07 +0500

    exponentions

---
+++ Operators/exponentions.js
@@ -0,0 +1,4 @@
+let nb1 = 3;
+let nb2 = 2;
+let result = 3 ** 2;
+console.log(result);
---
+++ Operators/modulus.js
@@ -0,0 +1,9 @@
+nb1 = 10;
+nb2 = 3;
+result = nb1 % nb2;
+console.log(result);
+
+nb1 = 9;
+nb2 = 3;
+result = nb1 % nb2;
+console.log(result);
