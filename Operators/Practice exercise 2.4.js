d7bfd0bcc27ae8c9777db044bfe0003f6cab7639
Noor ul Ain <57191809+nooruluzair@users.noreply.github.com>
2022-11-02 20:17:55 +0500

    assignment operator

---
+++ Operators/Practice exercise 2.4.js
@@ -0,0 +1,7 @@
+let a = 4;
+let b = 6;
+let c = 8;
+a = b + a;
+a = a / c;
+c = c % b;
+console.log(a,b,c);
---
+++ Operators/comparision operator.js
@@ -0,0 +1,7 @@
+let x = 6;
+let y = 6
+let z = "6";
+console.log(x === y); // check data and type also
+console.log(x === z); // checks data and type also
+console.log(x == y); // only cheks data not type
+console.log(x == z); // only checks data not type
