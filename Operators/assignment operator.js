a7be30ab16ef148f2ae8e243ed330bab178ae782
Noor ul Ain <57191809+nooruluzair@users.noreply.github.com>
2022-11-02 19:35:58 +0500

    Create assignment operator.js

---
+++ Operators/assignment operator.js
@@ -0,0 +1,13 @@
+let x = 5;
+x += 7; //this is x = x +7 that is 5 + 7
+console.log(x)
+x -= 2;
+console.log(x);
+x *= 6;
+console.log(x);
+x /= 3;
+console.log(x);
+x **= 2;
+console.log(x);
+x %= 3;
+console.log(x);
