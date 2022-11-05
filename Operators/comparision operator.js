610f809e81e7f6c25e306c85e2fab27a43789716
Noor ul Ain <57191809+nooruluzair@users.noreply.github.com>
2022-11-05 18:32:13 +0500

    Update comparision operator.js

--- Operators/comparision operator.js
+++ Operators/comparision operator.js
@@ -5,3 +5,8 @@ console.log(x === y); // check data and type also
 console.log(x === z); // checks data and type also
 console.log(x == y); // only cheks data not type
 console.log(x == z); // only checks data not type
+
+let a = 5;
+let b = "5";
+console.log(a != b);  // checks for non equality loosely
+console.log(a !== b); //checks for non equality with type
