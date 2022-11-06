13a7b54cbd8be043a10adb2cf7334cc627df2539
Noor ul Ain <57191809+nooruluzair@users.noreply.github.com>
2022-11-06 15:49:56 +0500

    logical operators

---
+++ Logical Operator/logical Operators.js
@@ -0,0 +1,12 @@
+let x = 4;
+let y = 6;
+let z = 10;
+console.log(x < y && x > y); // And Logic
+console.log(z > y && y < z); // And Logic
+console.log(x < y || x > y); // OR Logic 
+console.log(z > y || y < z); // OR Logic
+
+
+let a = false;
+console.log(!a); // Simply flips
+console.log(!(x < y)); // x is less than why but due to ! it outputs false
---
+++ miles to kilometer/miles to kilometers.js
@@ -0,0 +1,7 @@
+// For reference, 1 mile equals 1.60934 kilometers
+
+let ValueInMiles = window.prompt("Please enter value in miles >>");
+let ValueInKilometers = (ValueInMiles * 1.60934);
+ValueInMiles = Number(ValueInMiles);
+ValueInKilometers = Number(ValueInKilometers);
+alert(ValueInKilometers);
