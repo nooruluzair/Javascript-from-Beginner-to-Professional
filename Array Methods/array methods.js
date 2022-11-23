c48a9dbaddf7219e0387dbbcb49fbb20a960bbe2
Noor ul Ain <57191809+nooruluzair@users.noreply.github.com>
2022-11-23 20:41:23 +0500

    Update array methods.js

--- Array Methods/array methods.js
+++ Array Methods/array methods.js
@@ -6,4 +6,9 @@ console.log(fruitnames);
 nameofcities = ["karachi","lahore", "islamabad","multan"];
 nameofcities.push("bahawalpur");
 console.log(nameofcities);
-    //
+    //adding element at a certain index
+nameOfCountries = ["Pakistan", "US", "Newzealand", "Australia"];
+nameOfCountries.splice(2,0, "India"); //adding india after 2 index and deleting 0 names after adding
+console.log(nameOfCountries);
+nameOfCountries.splice(2,1,"England");//adding england after index 2 but before deleting 1 element that is india
+console.log(nameOfCountries);
