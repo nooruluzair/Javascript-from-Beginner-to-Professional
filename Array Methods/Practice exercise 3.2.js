6a52034dc7744783f7431fc133632269e39284b9
Noor ul Uzair <noor.annie2768@gmail.com>
2022-12-14 23:59:27 +0500

    updated

--- Array Methods/Practice exercise 3.2.js
+++ Array Methods/Practice exercise 3.2.js
@@ -23,8 +23,18 @@ let indexofmilk = ShoppingList.indexOf("Milk");
 console.log(indexofmilk);
 
 //After Bananas, add Carrots and Lettuce.
+ShoppingList.splice(1,0, "Carrots","Lettuce");
+console.log(ShoppingList);
 
-/*Create a new list containing Juice and Pop.
-9. Combine both lists, adding the new list twice to the end of the first list.
-10. Get the last index value of Pop and output it to the console.*/
+//Create a new list containing Juice and Pop.
+let NewShoppingList = ["Juice","Pop"];
+console.log(NewShoppingList);
 
+//Combine both lists, adding the new list twice to the end of the first list.
+let combinelist = ShoppingList.concat(NewShoppingList,NewShoppingList);
+console.log(combinelist);
+
+//Get the last index value of Pop and output it to the console.
+
+let indexofpop = combinelist.indexOf("Pop");
+console.log(indexofpop);
